6bdfd699528591d12fb161102c331a8f68d889f7
Michael Comella <michael.l.comella@gmail.com>
2017-07-05 16:15:10 -0700

    Replace file headers with MPL.

--- Uno/UnoTests/UnoTests.swift
+++ Uno/UnoTests/UnoTests.swift
@@ -1,10 +1,6 @@
-//
-//  UnoTests.swift
-//  UnoTests
-//
-//  Created by Michael Comella on 7/5/17.
-//  Copyright © 2017 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import XCTest
 @testable import Uno
